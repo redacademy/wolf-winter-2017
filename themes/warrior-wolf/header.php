efbc3307eb4ebccef441d9643f4a2c2921d0591d
Clara <clara@PIKACHU-2.local>
2017-03-14 16:14:12 -0700

    removed img from header

--- themes/warrior-wolf/header.php
+++ themes/warrior-wolf/header.php
@@ -31,7 +31,6 @@
 
 					<nav id="site-navigation" class="main-navigation" role="navigation">
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?>
-						<!--<img src="./assets/mobile_hamburger.png">-->
 						</button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					</nav><!-- #site-navigation -->
