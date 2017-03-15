eea20b4e450cb09cb29822b8c4c8f387fda13ce7
Clara <clara@PIKACHU-2.local>
2017-03-15 11:32:43 -0700

    Implemented nav plugin

--- themes/warrior-wolf/js/menu-click.js
+++ themes/warrior-wolf/js/menu-click.js
@@ -1,6 +1,6 @@
-(function ($) {
-    $('.main-navigation').click(function() {
-        $('.menu').toggleClass('menu-click');
-        $('.menu ul').toggleClass('nav-overlay');
-    })
-})(jQuery);
+// (function ($) {
+//     $('.main-navigation').click(function() {
+//         $('.menu').toggleClass('menu-click');
+//         $('.menu ul').toggleClass('nav-overlay');
+//     })
+// })(jQuery);
