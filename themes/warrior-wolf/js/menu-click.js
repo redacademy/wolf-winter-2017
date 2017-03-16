80293f21079e043f150526aebfdf8cc0340cd475
Clara <clara@PIKACHU-2.local>
2017-03-16 13:43:44 -0700

    Added button color change on click

--- themes/warrior-wolf/js/menu-click.js
+++ themes/warrior-wolf/js/menu-click.js
@@ -1,5 +1,6 @@
 (function ($) {
     $('.main-navigation').click(function() {
+        $('.main-navigation').toggleClass('button-color-change');
         $('.mobile-menu').toggleClass('menu-click');
         $('.menu').toggleClass('nav-overlay');
     })
