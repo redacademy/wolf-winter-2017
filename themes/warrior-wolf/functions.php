9140a370fe56fa4081d4e46911d1b5f1be9774fb
njohnstud <n.e.johnston@hotmail.com>
2017-03-16 14:46:34 -0700

    Added bootstrap modal and tab functionality

--- themes/warrior-wolf/functions.php
+++ themes/warrior-wolf/functions.php
@@ -87,6 +87,8 @@ function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
 	wp_enqueue_script( 'jquery');
 
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/build/js/bootstrap.min.js', array(), false, true);
+
 	wp_enqueue_script( 'menu-click', get_template_directory_uri() . '/build/js/menu-click.min.js', array('jquery'), false, true);
 
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
--- themes/warrior-wolf/js/menu-click.js
+++ themes/warrior-wolf/js/menu-click.js
@@ -3,4 +3,25 @@
         $('.mobile-menu').toggleClass('menu-click');
         $('.menu').toggleClass('nav-overlay');
     })
+
+
+
+    $('#menu-item-38 a').on('click', function (e) {
+        e.preventDefault();
+        $('#contact-modal').modal({show: true});
+    })
+
+    $('#confirmation-modal').modal({show: false, backdrop: true});
+
+    $('#contact-modal').on('hidden.bs.modal', function () {
+        $('#confirmation-modal').show();
+    })
+
+    // $('#confirmation-modal').focus();
+
+    // $('.btn-contact-submit').on('hidden.bs.modal', function (e) {
+    //     e.preventDefault;
+    //     $('#confirmation-modal').focus();
+    // })
 })(jQuery);
+
