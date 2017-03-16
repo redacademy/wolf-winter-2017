47f569461f6ac5492fb969b2dde45f95bafd5db1
njohnstud <n.e.johnston@hotmail.com>
2017-03-16 15:52:07 -0700

    Deleted bootstrap, questioning life rn.

--- themes/warrior-wolf/js/menu-click.js
+++ themes/warrior-wolf/js/menu-click.js
@@ -4,17 +4,5 @@
         $('.mobile-menu').toggleClass('menu-click');
         $('.menu').toggleClass('nav-overlay');
     })
-
-    $('#menu-item-38 a').on('click', function (e) {
-        e.preventDefault();
-        $('#contact-modal').modal({show: true});
-    })
-
-    $('#confirmation-modal').modal({show: false, backdrop: true});
-
-    $('#contact-modal').on('hidden.bs.modal', function () {
-        $('#confirmation-modal').show();
-    })
-
 })(jQuery);
 
