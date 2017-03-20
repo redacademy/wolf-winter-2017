87f00dfcce4c1fe7b838587c6e205894cb14fba2
Clara <clara@PIKACHU-2.local>
2017-03-20 16:38:50 -0700

    modified menu-click function

--- themes/warrior-wolf/js/menu-click.js
+++ themes/warrior-wolf/js/menu-click.js
@@ -1,5 +1,6 @@
 (function ($) {
     $('.main-navigation').click(function() {
+        $('.site-header').toggleClass('header-border-change');
         $('.main-navigation').toggleClass('button-color-change');
         $('.mobile-menu').toggleClass('menu-click');
         $('.menu').toggleClass('nav-overlay');
