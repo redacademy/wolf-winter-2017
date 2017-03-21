eaea8db7179f43a12e152624d8e483146b4cad14
Clara <clara@PIKACHU-2.local>
2017-03-21 09:43:04 -0700

    added widgets

---
+++ plugins/certifications-widget/views/admin.php
@@ -0,0 +1,8 @@
+<!-- This file is used to markup the administration form of the widget. -->
+
+<div class="widget-content">
+   <p>
+        <label for="<?php echo $this->get_field_id('certifications'); ?>">Certifications</label>
+        <input class="widefat" id="<?php echo $this->get_field_id('certifications'); ?>" name="<?php echo $this->get_field_name('certifications'); ?>" type="text" value="<?php echo $certifications; ?>">
+   </p>
+</div>
---
+++ plugins/certifications-widget/views/widget.php
@@ -0,0 +1,7 @@
+<!-- This file is used to markup the public-facing widget. -->
+<?php if ( strlen( trim( $certifications ) ) > 0 ): ?>
+    <div class="textwidget"> 
+        <span><?php echo $certifications;?></span>
+        <span class="test">Professional Certifications</span>
+    </div>
+<?php endif; ?>
