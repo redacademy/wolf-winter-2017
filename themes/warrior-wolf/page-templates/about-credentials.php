fd6a067cdf3b55501961aa3d65df0ceb63803255
njohnstud <n.e.johnston@hotmail.com>
2017-03-22 15:30:01 -0700

    Added styling for credentials.

--- themes/warrior-wolf/page-templates/about-credentials.php
+++ themes/warrior-wolf/page-templates/about-credentials.php
@@ -18,13 +18,29 @@
   <div class="about-nav-wrapper">
     <ul class='etabs'>
       <li class='tab'><a href="<?php echo get_template_directory_uri(); ?>/page-templates/about.php">Bio</a></li>
-      <li class='tab'><a href="//localhost:3000/warrior-wolf/about/credentials">Credentials</a></li>
+      <li class='tab'><a href="//localhost:3000/warrior-wolf/about-me/credentials">Credentials</a></li>
       <li class='tab'><a href="<?php echo get_template_directory_uri(); ?>/about-method.php">Method</a></li>
       <li class='tab'><a href="<?php echo get_template_directory_uri(); ?>/about-mission.php">Mission</a></li>
     </ul>
   </div>
 </div>
 <section class="content-wrapper">
+
+
+  <?php
+/*
+    A loop field named "gallery" with sub-fields "slide_title" and "upload"
+    Loop fields return an associative array containing *ALL* sub-fields and their values
+    NOTE: Values of sub-loop fields are returned when using get() on the parent loop!
+*/
+$fields = CFS()->get( 'credential' );
+foreach ( $fields as $field ) {?>
+    <li>
+    <?php echo $field['credential_title']; ?>
+    <?php echo $field['credential_date']; ?>
+          </li>
+<?php } ?>
+
 
 </section>
 <?php get_footer(); ?>
