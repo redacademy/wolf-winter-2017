91c0afc0b891a9465aa3b155fc2ab791615c81e0
Clara <clara@PIKACHU-2.local>
2017-03-22 18:20:01 -0700

    added fonts

--- themes/warrior-wolf/single-programs.php
+++ themes/warrior-wolf/single-programs.php
@@ -27,33 +27,57 @@ get_header(); ?>
 			<h2><?php echo get_the_title(); ?></h2>
 			<?php the_post_navigation(); ?>
 		</div>
+		<div class="full-program-container">
+			<h2>Overview</h2>
 
-		<h2>Overview</h2>
-		<div class="short-overview-container">
-			<p>Duration: <?php echo CFS()->get('duration'); ?></p>
-			<p>Location: <?php echo CFS()->get('location'); ?></p>
-		</div>
-		<div class="goals-container">
-			<p>At the end of the course, students will be able to:</p>
-			<?php $goals = CFS()->get( 'course_goals' );
-			foreach ( $goals as $goal ) { ?>
-    			<p> <?php echo $goal['goals'];?> </p>
-			<?php } ?>
-		</div>
-		<h2>required equipment</h2>
-		<div class="equipment-container"></div>
-		<h2>important</h2>
-		<div class="skills-container">
-			<h4>skills requirements</h4>
-			<p><?php echo CFS()->get('skill_requirements'); ?></p>
-		</div>
-		<div class="conditions-container">
-			<h4>snow conditions</h4>
-			<p><?php echo CFS()->get('conditions'); ?></p>
-		</div>
-		<div class="location-container">
-			<h4>location</h4>
-			<p><?php echo CFS()->get('location_info'); ?></p>
+			<div class="short-overview-container">
+				<p>Duration: <?php echo CFS()->get('duration'); ?></p>
+				<p>Location: <?php echo CFS()->get('location'); ?></p>
+			</div>
+
+			<div class="goals-container container">
+				<h4>At the end of the course, students will be able to:</h4>
+				<div class="goals-list-container">
+					<ul>
+						<?php $goals = CFS()->get( 'course_goals' );
+						foreach ( $goals as $goal ) { ?>
+    						<li> <?php echo $goal['goals'];?> </li>
+						<?php } ?>
+					</ul>
+				</div>
+			</div>
+
+			<h2>required equipment</h2>
+
+			<div class="equipment-container"></div>
+		</div> <!-- .full-program-container -->
+
+		<div class="full-important-container">
+			<h2>important</h2>
+
+			<div class="skills-container container">
+				<div class="number-circle">
+					<h3>1</h3>
+				</div>
+				<h4>skills requirements</h4>
+				<p><?php echo CFS()->get('skill_requirements'); ?></p>
+			</div>
+
+			<div class="conditions-container container">
+				<div class="number-circle">
+					<h3>2</h3>
+				</div>
+				<h4>snow conditions</h4>
+				<p><?php echo CFS()->get('conditions'); ?></p>
+			</div>
+
+			<div class="location-info-container container">
+				<div class="number-circle">
+					<h3>3</h3>
+				</div>
+				<h4>location</h4>
+				<p><?php echo CFS()->get('location_info'); ?></p>
+			</div>
 		</div>
 
 		</main><!-- #main -->
